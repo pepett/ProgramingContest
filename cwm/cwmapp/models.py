c6119d18b3f9045be4d9718d4bc85d5a9bd05d78
pepett <harukiramen@gmail.com>
2023-05-17 15:45:19 +0900

    model

--- cwm/cwmapp/models.py
+++ cwm/cwmapp/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 
+class User( models.Model ):
+    user_name = models.CharField( max_length = 50 )
+    user_mail = models.CharField( max_length = 50, primary_key = True )
+    user_pass = models.CharField( max_length = 50 )
+
+class Music( models.Model ):
+    music_name = models.CharField( max_length = 100 )
+    music_eval = models.IntegerField( default = 0 )
+    

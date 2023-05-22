3b652b9696fe3780e2f8abbb496e09cc272c7128
pepett <harukiramen@gmail.com>
2023-05-22 09:44:19 +0900

    models.py

--- cwm/cwmapp/models.py
+++ cwm/cwmapp/models.py
@@ -9,13 +9,18 @@ class User( models.Model ):#ユーザーのテーブル
 
 class Music( models.Model ):#曲のテーブル
     music_id = models.AutoField( primary_key = True )
-    music_name = models.CharField( max_length = 100 )
+    music_name = models.TextField()
     music_star = models.IntegerField( default = 0 )
+    music_image = models.CharField( max_length = 200 )
+    music_artistname = models.TextField()
+    music_albumname = models.TextField()
+    music_preview = models.TextField()
+    music_playurl = models.TextField()
     #music_nice = models.IntegerField( default = 0 )
     
-class GoodList( models.Model ):#いいね
-    good_music_id = models.IntegerField()
-    good_user_mail = models.EmailField()
+class LikeList( models.Model ):#いいね
+    like_music_id = models.IntegerField()
+    like_user_mail = models.EmailField()
 
 class HistoryList( models.Model ):#履歴
     history_music_id = models.IntegerField()
@@ -25,6 +30,11 @@ class Comment( models.Model ):#コメント
     comment_id = models.AutoField( primary_key = True )
     comment_text = models.TextField()
     comment_user_mail = models.EmailField()
+    comment_music_id = models.IntegerField()
+    comment_good = models.IntegerField()
 
 class Result( models.Model ):
     result_comment_id = models.IntegerField()
+    result_text = models.TextField()
+    result_user_mail = models.EmailField()
+    result_music_id = models.IntegerField()

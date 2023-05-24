ee54790b128a58ac0adfd9d31638244e84a8f1b8
PKNT228016 <k228016@kccollege.ac.jp>
2023-05-24 14:39:57 +0900

    edit views.py index

--- cwm/cwmapp/views.py
+++ cwm/cwmapp/views.py
@@ -26,12 +26,49 @@ def result( request ):
     return render( request, 'cwm/result.html' )
 
 def index( request ):
+        def truncate_string(text, max_length):
+            if len(text) <= max_length:
+                return text
+            else:
+                return text[:max_length] + "..."
+
+        max_length = 15
+
         lz_uri = 'spotify:artist:3wvCMqwyJachksGLF0kjMJ'
         spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id='811426a7bad2420c98d9f2d03a88141d',client_secret='62e88e7c02754132ab2af5f4078543fc',))
         results = spotify.artist_top_tracks(lz_uri)
         final_result=results['tracks']
+
+        lz_uri2 = 'spotify:artist:1snhtMLeb2DYoMOcVbb8iB'
+        spotify2 = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id='811426a7bad2420c98d9f2d03a88141d',client_secret='62e88e7c02754132ab2af5f4078543fc',))
+        results2 = spotify2.artist_top_tracks(lz_uri2)
+        final_result2=results2['tracks']
+
+        lz_uri3 = 'spotify:artist:5Vo1hnCRmCM6M4thZCInCj'
+        spotify3 = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id='811426a7bad2420c98d9f2d03a88141d',client_secret='62e88e7c02754132ab2af5f4078543fc',))
+        results3 = spotify3.artist_top_tracks(lz_uri3)
+        final_result3=results3['tracks']
+
+        j = 0
+        for i in final_result:
+            final_result[j]['name'] = truncate_string(i['name'],max_length)
+            final_result[j]['artists'][0]['name'] = truncate_string(i['artists'][0]['name'],max_length)
+            j = j + 1
+
+        j = 0
+        for i in final_result2:
+            final_result2[j]['name'] = truncate_string(i['name'],max_length)
+            final_result2[j]['artists'][0]['name'] = truncate_string(i['artists'][0]['name'],max_length)
+            j = j + 1
+
+        j = 0
+        for i in final_result3:
+            final_result3[j]['name'] = truncate_string(i['name'],max_length)
+            final_result3[j]['artists'][0]['name'] = truncate_string(i['artists'][0]['name'],max_length)
+            j = j + 1
+
         for tracks in final_result:
-            return render(request,'cwm/index.html',{"results":final_result})
+            return render(request,'cwm/index.html',{"results":final_result,"results2":final_result2,"results3":final_result3})
 
 def music( request ):
     return render( request, 'cwm/music.html' )
--- cwm/static/js/index.js
+++ cwm/static/js/index.js
@@ -4,16 +4,18 @@ window.onload = ()=>{
 let IsPlayMusic = '';
 let Playing = 0;
 
-function PlayMusic(SongURL){
+function PlayMusic(SongURL,PlayBTN){
     if(SongURL == 'None'){
         alert(SongURL);
         return;
     }
     if(IsPlayMusic.src == SongURL){
     if(!IsPlayMusic.paused){
+        IsPlayTex(PlayBTN);
         IsPlayMusic.pause();
         console.log('pause')
     }else{
+        IsPlayTex(PlayBTN);
         IsPlayMusic.play();
         console.log('play')
         Playing = 1;
@@ -22,9 +24,11 @@ function PlayMusic(SongURL){
         if(Playing == 0){
         IsPlayMusic = new Audio(SongURL);
         IsPlayMusic.play();
+        IsPlayTex(PlayBTN);
         Playing = 1
         console.log('playnew : '+IsPlayMusic.src);
         }else{
+            IsPlayTex(PlayBTN);
             IsPlayMusic.pause();
             console.log('pause')
             Playing = 0;
@@ -32,12 +36,13 @@ function PlayMusic(SongURL){
     }
 }
 function IsPlayTex(PlayBTN){
-    console.log(PlayBTN.classList);
     if(PlayBTN.classList == "PlaySongBT"){
         PlayBTN.classList.remove("PlaySongBT");
         PlayBTN.classList.add("StopSongBT");
+        PlayBTN.innerHTML = StopBTtex;
     }else{
         PlayBTN.classList.remove("StopSongBT");
         PlayBTN.classList.add("PlaySongBT");
+        PlayBTN.innerHTML = PlayBTtex;
     }
 };

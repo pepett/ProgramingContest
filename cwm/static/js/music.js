049c53918e56f42f4ac4b9f0bc07d4b96518d543
pepett <harukiramen@gmail.com>
2023-05-30 15:18:20 +0900

    曲画面のスタイル

--- cwm/static/js/music.js
+++ cwm/static/js/music.js
@@ -1,8 +1,27 @@
 let play_music = {
-    
+    audio: null,//Audio
+    is_play: false,//boolean
 };
 
-window.onload = () => {
 
+window.onload = () => {
 }
 
+const play = ( obj, url ) => {
+    if( play_music.audio == null ){
+        play_music.audio = new Audio( url );
+        play_music.audio.addEventListener("ended", ()=>{
+            play_music.is_play = false;
+            obj.innerHTML = PlayBTimg;
+        });
+    }
+    if( play_music.is_play ){
+        play_music.audio.pause();
+        play_music.is_play = false;
+        obj.innerHTML = PlayBTimg;
+    }else{
+        play_music.audio.play();
+        play_music.is_play = true;
+        obj.innerHTML = StopBTimg;
+    }
+}

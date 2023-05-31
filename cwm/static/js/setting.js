9ff3a17dda42f5e0dab7613bca1185a04e1d66ee
PKNT228016 <k228016@kccollege.ac.jp>
2023-05-31 15:19:10 +0900

    setting.js html css

---
+++ cwm/static/js/setting.js
@@ -0,0 +1,39 @@
+window.onload = ()=>{
+    UserRadioBox = document.getElementsByClassName("UserRadioBox");
+    UserFavorite = document.getElementById("UserFavorite");
+    UserHistory = document.getElementById("UserHistory");
+    UserProFile = document.getElementById("UserProFile");
+    checkbox();
+}
+UserRadioBox = document.getElementsByClassName("UserRadioBox");
+UserFavorite = document.getElementById("UserFavorite");
+UserHistory = document.getElementById("UserHistory");
+UserProFile = document.getElementById("UserProFile");
+
+UserRadioBox[0].addEventListener('click', () =>{
+    checkbox();
+});
+UserRadioBox[1].addEventListener('click', () =>{
+    checkbox();
+});
+UserRadioBox[2].addEventListener('click', () =>{
+    checkbox();
+});
+
+function checkbox(){
+    if(UserRadioBox[0].checked){
+        UserFavorite.style.display = '';
+        UserHistory.style.display = 'none';
+        UserProFile.style.display = 'none';
+    }
+    if(UserRadioBox[1].checked){
+        UserFavorite.style.display = 'none';
+        UserHistory.style.display = '';
+        UserProFile.style.display = 'none';
+    }
+    if(UserRadioBox[2].checked){
+        UserFavorite.style.display = 'none';
+        UserHistory.style.display = 'none';
+        UserProFile.style.display = '';
+    }
+};

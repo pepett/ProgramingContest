1747e495ccebd40e1aecc94bcc0e52be718052c9
PKNT228016 <k228016@kccollege.ac.jp>
2023-06-01 11:45:49 +0900

    edit index setting views base

--- cwm/static/js/index.js
+++ cwm/static/js/index.js
@@ -4,6 +4,14 @@ window.onload = ()=>{
 
 /*ローディングjsここから*/
 const loading = document.querySelector( '#loading' );
+
+let Btns = document.getElementsByClassName('PlaySongBT');
+for(let i=0;i < Btns.length;i++){
+    console.log(Btns[i].value)
+    if(Btns[i].value == 'None'){
+        Btns[i].style.display = 'none';
+    }
+}
  
 window.addEventListener( 'load', () => {
   loading.classList.add( 'loaded' );
